4c61aacb1178d98ca56136cb7ce5fbf0a522a35d
Bhanu Khetharpal <79537130+bhanukhetharpal@users.noreply.github.com>
2022-05-29 13:31:02 +0530

    ups

--- lib/utils/text.dart
+++ lib/utils/text.dart
@@ -15,3 +15,8 @@ class modified_text extends StatelessWidget {
     );
   }
 }
+extension StringExtension on String {
+  String capitalize() {
+    return "${this[0].toUpperCase()}${this.substring(1).toLowerCase()}";
+  }
+}
