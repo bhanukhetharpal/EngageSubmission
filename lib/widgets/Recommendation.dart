3e04bb5ec63eb7527766c8187ccd36584e67bc28
Bhanu Khetharpal <79537130+bhanukhetharpal@users.noreply.github.com>
2022-05-29 13:29:18 +0530

    ups

--- lib/widgets/Recommendation.dart
+++ lib/widgets/Recommendation.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/utils/helper_widgets.dart';
+import 'package:myapp/utils/text.dart';
 import 'package:myapp/widgets/fetch.dart';
 import 'package:myapp/widgets/globals.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Recommendations extends StatefulWidget {
-
   Recommendations({Key? key}) : super(key: key);
   @override
   State<Recommendations> createState() => _RecommendationsState();
@@ -15,61 +16,73 @@ class _RecommendationsState extends State<Recommendations> {
   List<String> favmovies = Globals.getMyFavMovies();
   @override
   void initState() {
-   recmovies = loadmovies();
+    recmovies = loadmovies();
     super.initState();
   }
-  Future<List<Movie>> loadmovies() async{
+
+  Future<List<Movie>> loadmovies() async {
     List<Movie> list = [];
-    for(var i in favmovies){
-    list.addAll(await fetchMovies(i));
+    for (var i in favmovies) {
+      list.addAll(await fetchMovies(i));
     }
+    list.sort((a, b) => b.Match_Score.compareTo(a.Match_Score)); //sorting the recommendations with the highest match score
     return list;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: Colors.transparent,
+      appBar: AppBar(
+        backgroundColor: Colors.red[800],
+        title: const modified_text(
+            text: 'You Might Also Want To Watch',
+            size: 20.0,
+            color: Colors.white),
+        centerTitle: true,
+      ),
       body: FutureBuilder<List<Movie>>(
         future: recmovies,
         builder: (context, snapshot) {
-      if (snapshot.hasData) {
-        return ListView.builder(
-          itemCount: snapshot.data!.length,
-          itemBuilder: (_, index) => Container(
-            child: Container(
-              margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-              padding: EdgeInsets.all(20.0),
-              decoration: BoxDecoration(
-                color: Colors.lightGreen[200],
-                borderRadius: BorderRadius.circular(15.0),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-              Text(
-              "Movie Name: ${snapshot.data![index].Name}",
-                style: TextStyle(
-                  fontFamily: 'Koulen',
-                  fontSize: 18.0,
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (_, index) => Container(
+                child: Container(
+                  margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  padding: EdgeInsets.all(20.0),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[850],
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      modified_text(
+                          text: "${snapshot.data![index].Name.toUpperCase()}",
+                          color: Colors.white,
+                          size: 18.0),
+                      addVerticalSpace(5),
+                      modified_text(
+                          text: "Genres : ${snapshot.data![index].Genres}",
+                          color: Colors.white70,
+                          size: 15.0),
+                      addVerticalSpace(5),
+                  modified_text(
+                      text: "${snapshot.data![index].Match_Score}%match",
+                      color: Colors.green,
+                      size: 15.0),
+                    ],
+                  ),
                 ),
               ),
-                addVerticalSpace(10),
-                  Text("Genres: ${snapshot.data![index].Genres}"),
-                  addVerticalSpace(10),
-                 Text("Match Score: ${snapshot.data![index].Match_Score}"),
-                ],
-              ),
-            ),
-          ),
-        );
-      } else {
-        return Center(child: CircularProgressIndicator());
-      }
-    },
-    ),
+            );
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
     );
   }
 }
-
